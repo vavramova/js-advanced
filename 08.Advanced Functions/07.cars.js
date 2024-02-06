0eee0f6c68277500e5083fda65f2b831844cae42
Viktoria <avramovaviki017@gmail.com>
2024-02-06 20:59:01 +0200

    Advanced Functions

---
+++ 08.Advanced Functions/07.cars.js
@@ -0,0 +1,49 @@
+function solve(input) {
+    const state = {};
+
+    function create(name, inherit, parentName) {
+        const obj = Object.create(inherit ? state[parentName] : null);
+        state[name] = obj;
+    }
+
+    function set(name, propName, value) {
+        state[name][propName] = value;
+    }
+
+    function print(name) {
+        const obj = state[name];
+        const props = getAllProperties(obj);
+        console.log(props.map(e => `${e[0]}:${e[1]}`).join(','));
+    }
+
+    function getAllProperties(obj) {
+        let props = [];
+        for (let currentObj = obj; currentObj !== null; currentObj = Object.getPrototypeOf(currentObj)) {
+            props = props.concat(Object.entries(currentObj));
+        }
+        return props;
+    }
+
+    input.forEach(element => {
+        let [command, ...tokens] = element.split(' ');
+        if(command == 'create') {
+                create(...tokens);
+        }else if(command =='set'){
+                set(...tokens);
+    }else if(command =='print'){
+                print(...tokens);
+    }
+        });
+}
+
+solve(['create c1',
+
+'create c2 inherit c1',
+
+'set c1 color red',
+
+'set c2 model new',
+
+'print c1',
+
+'print c2'])
